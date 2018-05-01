3d753733c2933f8611b6af2f8474dcdd217dd95a
herbramirez <ramirez_herbert@me.com>
2018-05-01 12:22:09 -0400

    Add files via upload

--- code2/tableviz/HRTile.js
+++ code2/tableviz/HRTile.js
@@ -56,6 +56,7 @@ class HRTile extends Tile {
             var x = 25;
             var h = 10;
             //stock
+            text('Dont have to use my AK', x*2, x*1.5);
             rect(x*2, h*5, 10,40);
             //handle
             rect(x*3.2, h*5, 5,40);
@@ -69,11 +70,20 @@ class HRTile extends Tile {
         
         }
         
+        //Birthday cake
         
          for (let i = 0; i <= this.event; i++){
             fill(255);
-             
-             rect(90,90,100,100);
+            stroke(0);
+            ellipse(x*6,x*10,100,100);
+            noStroke();
+            ellipse(x*6,x*11,100,100);
+            fill(random(255,255,255));
+            fill(255,0,0);
+             rect(x*5,x*8, 10,50);
+             rect(x*6,x*7, 10,40);
+             rect(x*7,x*8, 10,60);
+            //candles
         
         }
         
